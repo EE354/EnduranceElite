86a81ed0a6423c248cf781c960258e483a7b236c
Jermaine Hipolito <jermaine.hipolito@edu.sait.ca>
2023-05-16 09:50:04 -0600

    Nav Bar update

--- client/src/NavBar.svelte
+++ client/src/NavBar.svelte
@@ -84,9 +84,9 @@
             Dropdown link
           </a>
           <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            <a class="dropdown-item" href="#">Action</a>
-            <a class="dropdown-item" href="#">Another action</a>
-            <a class="dropdown-item" href="#">Something else here</a>
+            <a class="dropdown-item" href="/">Action</a>
+            <a class="dropdown-item" href="/">Another action</a>
+            <a class="dropdown-item" href="/">Something else here</a>
           </div>
         </li>
       </ul>
