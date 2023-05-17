6a467714c8bba3e7b9193f0459d93f6d55c412e2
Jermaine Hipolito <jermaine.hipolito@edu.sait.ca>
2023-05-17 13:55:56 -0600

    Added styling and centering

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,12 +1,9 @@
 <script>
-	export let name;
 	import NavBar from './NavBar.svelte'
 </script>
 
 <main>
 	<NavBar/>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
 <style>
@@ -17,12 +14,6 @@
 		margin: 0 auto;
 	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
 
 	@media (min-width: 640px) {
 		main {
--- client/src/NavBar.svelte
+++ client/src/NavBar.svelte
@@ -1,9 +1,18 @@
 <script>
+// On load, calculate and change position for each dropdown menu to override to center
+window.onload = function() {
+  
+    forEach(function(dropdownMenu) {
+        var dropdownMenuWidth = dropdownMenu.offsetWidth;
+        var leftPosition = (window.innerWidth - dropdownMenuWidth) / 2;
+        dropdownMenu.style.left = leftPosition + 'px';
+  });
+};
+
 
 </script>
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="/">Navbar</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -14,9 +23,9 @@
             <a class="nav-link" href="/">Home</a>
             </li>
             <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                About
-                </a>
+                <a class="nav-link" href="/" id="navbarDropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                Aboutd
+                 </a>
                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                 <a class="dropdown-item" href="/">Action</a>
                 <a class="dropdown-item" href="/">Another action</a>
@@ -25,7 +34,7 @@
                 </div>
             </li>
             <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <a class="nav-link" href="/" id="navbarDropdown" role="button" aria-haspopup="true" aria-expanded="false">
                 Activities
             </a>
             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
@@ -42,7 +51,7 @@
                 <a class="nav-link" href="/">Merchandise</a>
             </li>
             <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <a class="nav-link" href="/" id="navbarDropdown" role="button"  aria-haspopup="true" aria-expanded="false">
                 Events
                 </a>
                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
@@ -62,8 +71,80 @@
 
 
 <style>
+    @import url('https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans&display=swap');
+
+    .dropdown:hover .dropdown-menu {
+        display: block;
+    }
+
+    .dropdown-menu {
+        /* Drop-down */
+
+
+        box-sizing: border-box;
+        /* Auto layout */
+
+
+        background: rgba(0, 0, 0, 0.04);
+        /* Black */
+
+        border-bottom: 1px solid #000000;
+
+        /* Inside auto layout */
+
+        flex: none;
+        order: 2;
+        align-self: stretch;
+        flex-grow: 0;
+        z-index: 0;
+    }
+
+    a {
+
+        width: 100px;
+        height: 24px;
+
+        font-family: 'Plus Jakarta Sans';
+        font-style: normal;
+        font-weight: 500;
+        font-size: 16px;
+        line-height: 150%;
+        /* identical to box height, or 24px */
+
+        display: flex;
+        align-items: center;
+        text-align: center;
+        letter-spacing: -0.03em;
+
+        /* Black */
+
+        color: #000000;
+
+        opacity: 0.9;
+
+        /* Inside auto layout */
+
+        flex: none;
+        order: 1;
+        flex-grow: 0;
+    }
 
     @media (min-width: 992px) {
+
+        .collapse {
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
+            width: 992px;
+        }
+
+        .navbar {
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
+            width: 992px;
+        }
+
         .headerLeft {
             /* Headers */
 
@@ -111,5 +192,24 @@
             flex-grow: 1;
             z-index: 1;
         }
+
+        .dropdown-menu {
+            
+            flex-direction: row;
+            align-items: flex-start;
+            padding: 32px 80px 48px 170px;
+            gap: 10px;
+
+            width: 992px;
+            height: 250px;
+
+            position: fixed;
+            top: 10%;
+            left: 50%;
+            transform: translateX(-50%);
+
+        }
+
+        
     }   
 </style>
