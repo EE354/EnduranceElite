315fad831847ec86e8023048d2cbe736209b5681
cmbackflip <colem1700@gmail.com>
2023-05-16 19:41:34 -0600

    remove uri from config

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
 module.exports = {
     port : process.env.PORT || 3000,
-    mongoURI : process.env.MONGO_URI || "mongodb+srv://admin:zL1maxEtXmBVm5@cluster0.cd9r8fk.mongodb.net/?retryWrites=true&w=majority",
+    mongoURI : process.env.MONGO_URI,
 }
+
+
