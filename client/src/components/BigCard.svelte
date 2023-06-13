aebbc5e7597ff9a1d69d8df1bdac7a1257b0e603
Jermaine Hipolito <jermaine.hipolito@edu.sait.ca>
2023-06-13 08:31:30 -0600

    Created Big Card

---
+++ client/src/components/BigCard.svelte
@@ -0,0 +1,63 @@
+<script>
+    import Button from "../components/Button.svelte";
+
+</script>
+
+<div id="content-container">
+    
+    <div id="header-text">
+        <slot name="title"></slot>
+        <slot name="body"></slot>
+    </div>
+    <div class="sub">
+        <div class="subtitle"><slot name="subtitle1"></slot></div>
+        <div class="subtitle"><slot name="subtitle2"></slot></div>
+        <div class="subtitle"><slot name="subtitle3"></slot></div>
+    </div>
+    <div class="sub">
+        <div class="subbody"><slot name="subbody1"></slot></div>
+        <div class="subbody"><slot name="subbody2"></slot></div>
+        <div class="subbody"><slot name="subbody3"></slot></div>
+    </div>
+</div>
+
+<style>
+    subtitle {
+
+    }
+
+    #content-container {
+        /* Box */
+
+        /* Positioning */
+        padding: 24px;
+
+        /* Dimensions */
+        max-width: 621px;
+        height: 415px;
+
+        /* Properties */
+        border-radius: 32px;
+        background-color: #35CAD5;
+        box-shadow: 5px 10px 25px rgb(155, 155, 155);
+
+    }
+
+    #header-text {
+        /* Flex Vertically */
+        display: flex;
+        flex-direction: column;
+    }
+
+    .sub {
+
+        display: flex;
+        flex-direction: row;
+        /* Dimensions */
+    }
+
+    .subbody {
+        margin-right: 8px;
+        width: 33%;
+    }
+</style>
