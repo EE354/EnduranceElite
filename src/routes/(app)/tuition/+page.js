063db9a162de021e12ba0a7cb32a86041ce1fe16
Jermaine Hipolito <jermaine.hipolito@edu.sait.ca>
2023-06-23 14:09:19 -0600

    Created tuition page

---
+++ src/routes/(app)/tuition/+page.js
@@ -0,0 +1,18 @@
+export const load = async ({ locals}) => {
+
+    //Set page data
+    const cards = [
+        {
+            title: "Membership Fee",
+            description: "An annual membership fee is collected from all students enrolled in classes. Benefits of Membership Fees include the ability to enroll in any class or program and discounts on Special Activities such as Open Gym, Parent’s Night Out, Friday Night Flick & Day Camps.",
+            subtitle1: "Each Member Pays:",
+            subtitle2: "$45/Anually",
+            subtitle3: "",
+            subdesc1: "",
+            subdesc2: "*Your annual registration will be charged on the 15th of the month you registered.",
+            subdesc3: "Annual registration fees are charged to the card on file. Annual registration fees are non-refundable",
+        },
+    ]
+
+    return cards;
+};
