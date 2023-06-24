7175bfcb0e8f2bf6569df37d9468ec7fbc3e402d
Jermaine Hipolito <jermaine.hipolito@edu.sait.ca>
2023-06-23 19:32:53 -0600

    Entered information into the cards

--- src/routes/(app)/tuition/+page.svelte
+++ src/routes/(app)/tuition/+page.svelte
@@ -1,31 +1,36 @@
 <script>
     export let data;
 </script>
-<div class="container ">
+<div class="container">
     
-    <div>
+    <div class="container text-center bg-primary-500 p-4 grid grid-flow-col-dense grid-rows-2">
         <!-- Header Block -->
         <h1>Tuition</h1>
+        <img 
+            class="rounded-2xl w-[16rem] max-h-[16rem] row-span-2"
+            alt=""
+            src="$lib/Regular/tuition.jpg"
+        >
         <p>All classes and teams at Endurance Elite Gymnastics & Dance are paid monthly. Automatic payment through our Parent Portal is mandatory for registration into a class. Your child will be continuously enrolled and monthly tuition will be charged to your card on file. You never have to worry about pre-registration or losing your spot again! Listed below you will find our tuition rates, discounts & policies. Any addition class taken by a student or their immediate family member will receive a 10% discount on EVERY addition class (not just one!) Discounts are also available for Military families.Policies are subject to change, however, you will be notified via email through the parent portal.</p>
     </div>
     
-    {#each data.cards as {title, description, subtitle1, subtitle2, subtitle3, subdesc1, subdesc2, subdesc3}}
-            <div class="card container w-[600rem]">
-                <h2 class="bg-primary-500">{title}</h2>
+    <div class="container flex flex-wrap">
+
+    {#each data.cards as {title, description, header1, header2, header3, note1, note2, note3}}
+            <div class="card container w-[40rem] min-h-[24rem] grid grid-flow-row-dense grid-cols-3 m-4 p-4">
+                <h2 class="col-span-3 text-4xl">{title}</h2>
                 <p class="col-span-3">{description}</p>
-                <div class="flex">
-                    <h4 class="bg-primary-500">{subtitle1}</h4>
-                    <h4>{subtitle2}</h4>
-                    <h4>{subtitle3}</h4>
-                </div>
-                <div class="flex">
-                    <p>{subdesc1}</p>
-                    <p>{subdesc2}</p>
-                    <p>{subdesc3}</p>
-                </div>
+                <h4 class="">{header1}</h4>
+                <h4>{header2}</h4>
+                <h4>{header3}</h4>
+                <p>{note1}</p>
+                <p>{note2}</p>
+                <p>{note3}</p>
             </div>
     {/each}
-    
+ 
+</div>
+
         <!-- <h2 slot="title" style="font-size: 40px;">Membership Fee</h2>
         <p slot="body">An annual membership fee is collected from all students enrolled in classes. Benefits of Membership Fees include the ability to enroll in any class or program and discounts on Special Activities such as Open Gym, Parent’s Night Out, Friday Night Flick & Day Camps.</p>
         <h4 slot="subtitle1">Each Member Pays:</h4>
