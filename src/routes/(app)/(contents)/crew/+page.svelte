c70cb22f2a2179433ec701e8e1d3c62fee4b974d
Jermaine Hipolito <jermaine.hipolito@edu.sait.ca>
2023-06-24 12:26:08 -0600

    Created crew, jtc pages, moved content pages into a separate folder

---
+++ src/routes/(app)/(contents)/crew/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    import { Avatar } from '@skeletonlabs/skeleton';
+</script>
+
+{#each data.avatars as {src, name, roles, initials}}
+    <Avatar src="" width="w-32" rounded="rounded-full" initials=""/>
+    <h3>{name}</h3>
+    <p>{roles}</p>
+{/each}
