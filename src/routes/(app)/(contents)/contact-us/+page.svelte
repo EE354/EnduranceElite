0223b53fb138f52edfa73ca6d44b997d413f310a
Jermaine Hipolito <jermaine.hipolito@edu.sait.ca>
2023-06-24 16:17:59 -0600

    Created contact-us page

---
+++ src/routes/(app)/(contents)/contact-us/+page.svelte
@@ -0,0 +1,48 @@
+<main class="container flex">
+
+    <div class="card bg-primary-500 m-4 p-4 w-1/4">
+        <!-- Contact Information Block -->
+        <h4>Contact Information</h4>
+        <p>
+            Endurance Elite Gymnastics & Dance
+            <br/>15814 University Oak #114
+            <br/> Antonio, TX 78249
+            <br/>enduranceelitegym@gmail.com
+            <br/>(210) 769-6359
+        </p><br/>
+
+        <h4>Business Hours</h4>
+        <p>
+            Monday-Friday<br />8am-9am
+            Saturday<br />8am-2pm
+            Sunday<br />Special Events Only
+        </p>
+    </div>
+
+    <div class="container shadow-lg m-4 p-4">
+        <!-- Contract Form Block -->
+        <h4 class="cols-span2">We'd Love to Hear from You!</h4>
+        <form>
+            <label class="label">
+                <span>Name</span>
+                <div class="flex gap-4">
+                    <input class="input" type="text" placeholder="First Name" />
+                    <input class="input" type="text" placeholder="Last Name" />
+                </div>
+            </label>
+            <label class="label">
+                <span>Email Address</span>
+                <input class="input" type="text" />
+            </label>
+            <label class="label">
+                <span>Subject</span>
+                <input class="input" type="text" />
+            </label>
+            <label class="label">
+                <span>Comments</span>
+                <textarea class="textarea" rows="4" placeholder="Enter your message here." />
+            </label>
+            <button class="btn bg-primary-600 dark:bg-primary-900 w-28">Join</button>
+        </form>
+    </div>
+</main>
