12779aaa088d28add3b3387ab6f87dcb2e16040f
cmbackflip <colem1700@gmail.com>
2023-06-24 11:55:08 -0600

    Revert "Add signup toasts"
    This reverts commit 0888e68ad8b3cb3e89e4c0fab40cd935b827a0aa.

--- src/routes/(admin)/admin/events/+page.server.js
+++ src/routes/(admin)/admin/events/+page.server.js
@@ -1,10 +1,10 @@
 import {dateOfBirthSchema, eventDateSchema, nameSchema, stringSchema} from "$lib/server/zodSchemas.js";
 import {Event} from "$lib/server/models/Event";
-import {fail } from "@sveltejs/kit";
 
 
 export const load = async () => {
     const env = await Event.find({}).sort({timeStamp: 1});
+    console.log(env);
     const events = env;
     return {
         events: JSON.parse(JSON.stringify(env)),
@@ -23,7 +23,7 @@ export const actions = {
         const description = form.get("description");
         const location = form.get("location");
 
-        if (start > end) throw new Error({"message": "Start date must be before end date"});
+        if (start > end) throw new Error("Start date must be before end date");
 
 
         try {
@@ -45,93 +45,24 @@ export const actions = {
 
             return {
                 status: 200,
-                error: false,
                 message: "Event created successfully",
             }
         } catch (e) {
-
-            return fail(400, {
-                status: 400,
-                error: true,
-                message: JSON.parse(e.message),
-            });
-        }
-    },
-
-    delete: async ({request}) => {
-        const form = await request.formData();
-        const id = form.get("id");
-
-        try {
-            await Event.findByIdAndDelete(id);
-
             return {
-                status: 200,
-                message: "Event deleted successfully",
-            }
-        } catch (e) {
-            return fail(400, {
                 status: 400,
-                message: "Event failed to delete",
-            });
+                message: e.message,
+            }
         }
+
+
+
 
 
     },
-    edit: async ({request}) => {
-        const form = await request.formData();
-        const id = form.get("id");
+    delete: {
 
-        try {
-            const event = await Event.findById(id);
-            console.log(event)
-
-            return {
-                status: 200,
-                event: JSON.parse(event),
-                message: "Event ready for editing",
-            }
-        } catch (e) {
-            return fail(400, {
-                status: 400,
-                error: true,
-                message: "Failed to retrieve event",
-            });
-        }
     },
-    update: async ({request}) => {
-        const {session, user} = await locals.auth.validateUser();
+    update: {
 
-        const form = await request.formData();
-        const start = new Date(form.get("start"));
-        const end = new Date(form.get("end"));
-        const name = form.get("name");
-        const description = form.get("description");
-        const location = form.get("location");
-        const id = form.get("id");
-
-        if (start > end) throw new Error("Start date must be before end date");
-
-
-        try {
-            eventDateSchema.parse(start);
-            eventDateSchema.parse(end);
-            nameSchema.parse(name);
-            stringSchema.parse(description);
-            stringSchema.parse(location);
-
-            return {
-                status: 200,
-                error: false,
-                message: "Event created successfully",
-            }
-        } catch (e) {
-
-            return fail(400, {
-                status: 400,
-                error: true,
-                message: JSON.parse(e.message),
-            });
-        }
     }
 }
