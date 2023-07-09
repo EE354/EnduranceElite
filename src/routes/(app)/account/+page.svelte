baab379d67099a8bec89629cd87b7493321e3c02
Jermaine Hipolito <jermaine.hipolito@edu.sait.ca>
2023-07-08 18:43:52 -0600

    Created Group display on account page with search bar

--- src/routes/(app)/account/+page.svelte
+++ src/routes/(app)/account/+page.svelte
@@ -1,6 +1,16 @@
 <script>
     export let data;
 
+    let search;
+
+    $: searchedGroups = data.groups.filter(group => {
+        if (search != null ) {
+            return group.name.toLowerCase().includes(search.toLowerCase());
+        } 
+        return data.groups;
+    })
+
+    $: console.log(search);
 </script>
 <div class="container m-4 ml-8">
     <h1>
@@ -8,8 +18,21 @@
     </h1>
 
     <form method="POST" action="?/logout" class="mt-4">
-        <button type="submit" class="btn variant-filled-error">Logout</button>
+        <button type="submit" value="" class="btn variant-filled-error">Logout</button>
     </form>
+
+    <!-- List of Groups -->
+    <div class="card container flex flex-col h-[36rem] overflow-y-auto gap-10 p-10 bg-primary-500">
+        <h2>Join Groups!</h2>
+        <input bind:value={search} class="input" type="search" placeholder="Search Groups..."/>
+        {#each searchedGroups as group}
+            <div class="card container p-4 shadow-lg">
+                <h4>{group.name}</h4>
+                <p>{group.description}</p>
+                <button type="submit" class="btn bg-primary-600">Join</button>
+            </div>
+        {/each}
+    </div>
 
 </div>
 
