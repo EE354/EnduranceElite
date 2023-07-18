b373fb4fe42d78b8f8b06d50e43dae5545feb3c9
PiSauce <pisauce@hotmail.com>
2023-07-18 09:14:31 -0600

    changed event to schedule

--- src/routes/(admin)/admin/schedule/EditScheduleDrawer.svelte
+++ src/routes/(admin)/admin/schedule/EditScheduleDrawer.svelte
@@ -4,7 +4,7 @@
 
 <main class="grid place-items-center">
     <form method="POST" action="?/edit" use:enhance class="w-10/12">
-        <h3 class="p-5">Edit Event</h3>
+        <h3 class="p-5">Edit Schedule</h3>
         <hr class="!border-surface dark:!border-surface-50 mb-4">
 
         <label class="my-4">
@@ -21,19 +21,19 @@
             <input type="text" name="employee" class="input input-bordered" placeholder="Employee Name">
         </label>
         <label class="my-4">
-            <h5>Event Name</h5>
-            <input type="text" class="input input-bordered" name="name" placeholder="Event Name">
+            <h5>Schedule Name</h5>
+            <input type="text" class="input input-bordered" name="name" placeholder="Schedule Name">
         </label>
         <label class="my-4">
             <h5>Description</h5>
-            <textarea class="textarea" rows="4" name="description" placeholder="Event description" />
+            <textarea class="textarea" rows="4" name="description" placeholder="Schedule description" />
         </label>
         <label class="my-4">
             <h5>Location</h5>
-            <input type="text" name="location" class="input input-bordered" placeholder="Event Location">
+            <input type="text" name="location" class="input input-bordered" placeholder="Schedule Location">
         </label>
 
-        <button class="btn variant-filled-secondary ">Add Event</button>
+        <button class="btn variant-filled-secondary ">Add Schedule</button>
         <input class="btn variant-filled-error" type="reset" value="Clear">
     </form>
 </main>
