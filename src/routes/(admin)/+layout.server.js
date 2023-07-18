93ef80e245f696ddf1e43ba62b69cc22d397e833
PiSauce <pisauce@hotmail.com>
2023-07-18 09:32:57 -0600

    created file

---
+++ src/routes/(admin)/+layout.server.js
@@ -0,0 +1,14 @@
+import {redirect} from "@sveltejs/kit";
+
+
+export const load = async ({locals, url}) => {
+    const {session, user} = await locals.auth.validateUser();
+
+    console.log(user)
+
+    return {
+        status: 200,
+        session: session,
+        role: user?.role || 0,
+    }
+}
