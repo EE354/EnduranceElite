8f8712f7acba42da9ceeb5e038b2cd15cea7b2a1
Jermaine Hipolito <jermaine.hipolito@edu.sait.ca>
2023-07-25 09:33:57 -0600

    Added competitive pages

---
+++ src/routes/(app)/(contents)/(competitive)/college-recruiting/+page.svelte
@@ -0,0 +1,38 @@
+<script>
+    import { Avatar } from '@skeletonlabs/skeleton';
+</script>
+
+<main class="bg-secondary-500 bg-[url('$lib/Regular/background.jpg')]">
+    <div class="container text-center bg-primary-500 dark:bg-primary-900 p-4 mb-4 flex">
+        <!-- Header Block -->
+        <div>
+            <h1 class="text-primary-200 text-shadow italic">
+                COLLEGE RECRUITING
+            </h1>
+            <p>Introducing... Our amazing Competitive Team Members who look forward to a college career in gymnastics.</p>
+            <br><p>These individuals are dedicated to the sport of gymnastics, their academics, extra curricular activities and their families.</p>
+            <br><p>We are committed to helping these hard working athletes find their future college home.</p>
+            <br><p><b>Please email Coach Ashley at ashleyg.enduranceelitegym@gmail.com for more information.</b></p>
+        </div>
+        <img 
+            class="rounded-2xl w-[16rem] max-h-[16rem] object-cover"
+            alt="Ballerina Bending and Dancing"
+            src="$lib/Regular/Dance/dance.jpg"
+        >
+    </div>
+
+
+    <div class="container flex flex-wrap align-center justify-center">
+        <!-- Main Contents -->
+        <div class="flex gap-4 bg-neutral-100 shadow-lg m-4 p-4">
+            <!-- White Info Block -->
+            <Avatar src="" width="w-32" rounded="rounded-full"/>
+            <div>
+                <h3>Palvi Vagal</h3>
+                <p>Class of 2025</p>
+                <a href="https://www.instagram.com/palvi_gymnast.2025/?igshid=MzRlODBiNWFlZA%3D%3D" class="btn bg-primary-500">Palvi's Instagram</a>
+            </div>
+        </div>
+    </div>
+    
+</main>
---
+++ src/routes/(app)/(contents)/(competitive)/team-information/+page.svelte
@@ -0,0 +1,125 @@
+<script>
+    import { Avatar } from '@skeletonlabs/skeleton';
+</script>
+
+<main class="bg-secondary-500 bg-[url('$lib/Regular/background.jpg')]">
+    <div class="container text-center bg-primary-500 dark:bg-primary-900 p-4 mb-4 flex">
+        <!-- Header Block -->
+        <div>
+            <h1 class="text-primary-200 text-shadow italic">
+                TEAM INFORMATION
+            </h1>
+            <p>We offer Men & Women's Competitive Program through USA Gymnastics (Levels 3-10).</p>
+            <br><p>It is with great pride and a long time dream, that Endurance Elite has the ability to introduce a dual program to both the gymnastics and dance communities. At Endurance Elite, our goal is to create a “Super” Athlete, both successful in gymnastics and dance, therefore showing strength and equality in both sport and art form. We are creating a healthier, stronger, more aware athlete that can perform with confidence and grow through the sport with less injury and a better performance all around.</p>
+            <br><p>Through communication, hard work, and teamwork we are proud to offer a competitive team program fit for anyone looking to work hard and become part of a gym family.</p>
+        </div>
+        <img 
+            class="rounded-2xl w-[16rem] max-h-[16rem] object-cover"
+            alt="Ballerina Bending and Dancing"
+            src="$lib/Regular/Dance/dance.jpg"
+        >
+    </div>
+
+
+    <div class="container flex flex-wrap align-center justify-center">
+        <!-- Main Contents -->
+        <div class="card w-11/12 bg-primary-500 shadow-lg m-4 p-4">
+			<!-- Step Block -->
+			<span class="text-center w-100%"><h3>How to Become a Part of the Team</h3></span>
+			<div class="flex">
+				<div class="w-1/4">
+					<!-- Left Side of Block -->
+					<p>Three ways to become a part of the EE Team:</p>
+				</div>
+				<div class="divider-vertical w-1 h-100% bg-neutral-600 ml-1 mr-12" />
+				<div>
+					<!-- Right Side of Block -->
+					<ol class="list-decimal">
+						<li>Enroll in recreational classes and receive an invitation from your coach to enroll in a trial week of team.</li>
+						<li>Apply for a trial: if you are interested in switching gyms and/or teams we must receive the following before scheduling a trial:</li>
+                        <p>
+                            <br>* Detailed letter from Parent and/or Gymnast explaining reason for switching teams
+                            <br>* Copy of most recent financial statement stating a zero balance owed
+                            <br>* Letter of recommendation from previous coach
+                        </p>
+                        <br>
+						<li>Attend Annual Team Tryouts. Information will be posted in April for the next season's team tryouts.</li>
+					</ol>
+				</div>
+			</div>
+		</div>
+
+        <div class="bg-neutral-100 w-11/12 shadow-lg m-4 p-4">
+            <!-- White Info Block -->
+            <h3>Women's Team Level Progressions</h3><br>
+            <h4>Mini Elite- ages 3-4: 2 hours a week</h4>
+            <ul class="list-disc pl-4">
+                <li>Preschoolers that show potential in listening and strength.</li>
+                <li>Invite ONLY</li>
+                <li>No prior experience needed.</li>
+                <li>Focus: Strength, Shapes, Flexibility, Basic Skills.</li>
+            </ul><br>
+
+            <h4>Junior Elite- ages 4-6: 4 hours a week</h4>
+            <ul class="list-disc pl-4">
+                <li>Kinder kids that show potential in listening and strength or an interest in team.</li>
+                <li>Invite ONLY</li>
+                <li>No prior experience needed.</li>
+                <li>Focus: Strength, Shapes, Flexibility, Basic Skills.</li>
+            </ul><br>
+            
+            <h4>Jr. Preteam- ages 4-6: 6 hours a week</h4>
+            <ul class="list-disc pl-4">
+                <li>Kinder kids that have either come from Junior Elite or recommended based on skill level from a rec class.</li>
+                <li>Invite ONLY</li>
+                <li>Must have previous enrollment in Junior Elite or an Endurance Elite Rec Class.</li>
+                <li>Focus: Strength, Shapes, Flexibility, Basic Skills, Level 3 Skills.</li>
+            </ul><br>
+
+            <h4>Sr. Preteam- ages 7-10: 6 hours a week</h4>
+            <ul class="list-disc pl-4">
+                <li>School age kids that have either come from Junior Elite, Junior Preteam, or recommended based on skill level from a rec class.</li>
+                <li>Invite ONLY</li>
+                <li>Must have previous enrollment in Junior Elite, Jr. Preteam, or an Endurance Elite Rec Class.</li>
+                <li>Focus: Strength, Shapes, Flexibility, Basic Skills, Level 3 Skills.</li>
+            </ul><br>
+
+            <h4>Level 3- First Year Level 3 (non-compete) ages 5 & up: 12 hours a week.</h4>
+            <ul class="list-disc pl-4">
+                <li>Kinder and School age kids that have either come from Jr. Preteam or Sr. Preteam based on skill level.</li>
+                <li>MUST have Preteam experience no matter the age.</li>
+                <li>Focus: Strength, Shapes, Flexibility, Basic Skills, Level 3 Skills, Level 3 Routines.</li>
+            </ul><br>
+
+            <h4>Compulsory/Optional Team- ages 6 & up: 12-20 hours a week</h4>
+            <ul class="list-disc pl-4">
+                <li>MUST have Level 3 experience or more.</li>
+                <li>Focus: Competitive Gymnastics</li>
+            </ul><br>
+
+            <hr>
+
+            <h3>Men's Team Level Progressions</h3><br>
+            <h4>Preteam- ages 4-6: 6 hours a week</h4>
+            <ul class="list-disc pl-4">
+                <li>Preschool and Kinder kids, recommended from a Gymnastics Preschool/Rec and/or Ninja Class.</li>
+                <li>Invite ONLY</li>
+                <li>No prior experience needed</li>
+                <li>Focus: Strength, Shapes, Flexibility, Basic Skills, Level 3 Skills.</li>
+            </ul><br>
+
+            <h4>Level 3- ages 5 & up: 12 hours a week.</h4>
+            <ul class="list-disc pl-4">
+                <li>Kinder and School age kids that have either come from Preteam or invited from Gymnastics Rec and/or Ninja Class.</li>
+                <li>Invite ONLY</li>
+                <li>Focus: Strength, Shapes, Flexibility, Basic Skills, Level 3 Skills, Level 3 Routines</li>
+            </ul><br>
+        </div>
+
+        <div class="bg-primary-500 text-center">
+            <h2>For more info</h2>
+            <h3>on Women's and Men's Team</h3>
+            <h3>Email Ashley at ashleyg.enduranceelitegym@gmail.com to set up a meeting!</h3>
+        </div>
+    </div>
+</main>
