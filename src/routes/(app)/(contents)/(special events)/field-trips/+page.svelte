f70cae472f3a68e551a63389353e7b2d2fd4f6ae
Jermaine Hipolito <jermaine.hipolito@edu.sait.ca>
2023-07-28 13:52:55 -0600

    Added Summer Showcase, and Field Trips pages

--- src/routes/(app)/(contents)/(special events)/field-trips/+page.svelte
+++ src/routes/(app)/(contents)/(special events)/field-trips/+page.svelte
@@ -2,133 +2,67 @@
 	import { Avatar } from '@skeletonlabs/skeleton';
 </script>
 
-<main class="bg-secondary-500 bg-[url('$lib/Regular/background.jpg')]">
-    <div class="text-center bg-primary-500 dark:bg-primary-900 p-4 mb-4 lg:flex gap-4">
-        <!-- Header Block -->
-        <div>
-            <h1 class="text-primary-200 text-shadow italic">OPEN GYM</h1>
-            <p>
-                At open gym you and/or your child are free to use the equipment and play, work on skills,
-                dance or just hang out. There is no structured instruction and no spotting.
-            </p>
-            <br />
-            <p>
-                Coaches are in the gym to make sure you and/or your child are safe and to enforce the rules.
-                We ask that children 14 and younger have a parent present. Please do not drop off and leave
-                unsupervised.
-            </p>
-            <br />
-            <p>We have Open Gym for all age groups. Choose the one that fits your needs.</p>
-            <br />
-            <p class="underline font-bold">
-                All participants must create a parent portal before attending.
-            </p>
-            <a type="button" class="btn bg-primary-600 dark:bg-primary-900 ps-5 pe-5" href="https://app.iclasspro.com/portal/enduranceelite/dashboard"
-                >Parent Portal</a
-            >
-        </div>
-        <img
-            class="container rounded-2xl w-[24rem] mt-4 lg:mt-0 object-cover"
-            alt="Kids Playing in the Foam Pit"
-            src="$lib/Regular/SpecialEvents/opengym.jpg"
-        />
-    </div>
-
-    <div class="container md:flex align-center justify-center">
-        <!-- Main Contents -->
-        <div class="gap-4 bg-neutral-100 shadow-lg m-4 p-4 text-center">
-            <!-- White Info Block -->
-            <div class="bg-primary-200 p-4">
-                <h3>2022 Fall Schedule</h3>
-                <h4>Starting August 8, 2022</h4>
+<main class="pb-8">
+    <div class="container">
+        <div class="text-center bg-primary-500 dark:bg-primary-900 p-4 mb-4 lg:flex gap-4">
+            <!-- Header Block -->
+            <div>
+                <h1 class="text-primary-200 text-shadow italic">FIELD TRIPS</h1>
+                <p>NEED A NEW FIELD TRIP IDEA???</p>
+                <br />
+                <p>Endurance Elite is happy to provide a safe, fun, clean, enjoyable environment to get your students active and working in multiple programs such as gymnastics, dance, cheer and ninja. It’s never a dull moment in the gym!</p>
+                <br />
+                <p>Must Provide Transportation for Children.</p>
+                <br />
+                <p>Waivers are given to each school prior to field trip. Parents must sign for participation.</p>
+                <p>to schedule a time for your school's field trip.</p>
+                <br />
             </div>
-            <div class="bg-primary-600 p-4">
-                <p>1 Hour-Lil' Foot Open Gym</p>
-                <p>Ages: Babies-5 Years</p>
-                <br />
-                <p>Every Monday</p>
-                <p>9:00 - 10:00 am</p>
-                <br />
-                <p>Every Thursday</p>
-                <p>11:00 am - 12:00 pm</p>
-            </div>
-            <div class="bg-secondary-500 p-4">
-                <p>2 Hour Open Gym</p>
-                <p>All Ages</p>
-                <br />
-                <p>Every Tuesday & Friday</p>
-                <p>11:00 am - 1:00pm</p>
-                <br />
-                <p>Every Saturday</p>
-                <p>12:00 - 2:00 pm</p>
-            </div>
-            <div class="bg-primary-300 p-4">
-                <p>Adult Open Gym</p>
-                <p>Ages: 17+ Years</p>
-                <br />
-                <p>Every Wednesday</p>
-                <p>9:00 - 11:00 am</p>
-                <br />
-                <p>Every Thursday</p>
-                <p>7:00 - 9:00 pm</p>
-            </div>
+            <img
+                class="container rounded-2xl w-[24rem] mt-4 lg:mt-0 object-cover"
+                alt="Kids Playing in the Foam Pit"
+                src="$lib/Regular/SpecialEvents/opengym.jpg"
+            />
         </div>
 
-        <div class="card bg-primary-500 shadow-lg m-4 p-4">
+        <div class="card bg-primary-500 shadow-lg mx-4 mb-8 p-8">
             <!-- Step Block -->
-            <span class="text-center"><h3>OPEN GYM INFORMATION:</h3></span>
+            <h3>FIELD TRIP INFORMATION</h3>
+            <br/>
             <div class="md:flex">
                 <div class="w-11/12">
                     <!-- Left Side of Block -->
-                    <p class="underline">PRICES:</p>
+                    <p><b>Ages: 3-17</b></p>
+                    <br />
 
-                    <p><b>1 Hour Lil' Foot Open Gym</b></p>
+                    <p><b>Pricing:</b></p>
                     <ul>
-                        <li>$5 for Members</li>
-                        <li>$50 Member Punch Card (11 Visits)</li>
-                        <li>$8 for Non-Members</li>
-                        <li>$80 Non-Member Punch Card (11 Visits)</li>
+                        <li><p>$2 Hours - $10/Child</p></li>
+                        <li><p>$3 Hours - $15/Child</p></li>
+                        <li><p>$4 Hours - $20/Child</p></li>
                     </ul>
                     <br>
 
-                    <p><b>2 Hour Open Gym- All Ages</b></p>
-                    <ul>
-                        <li>$10 for Members</li>
-                        <li>$100 Member Punch Card (11 Visits)</li>
-                        <li>$15 for Non-Members</li>
-                        <li>$150 Non-Member Punch Card (11 Visits)</li>
-                    </ul>
-                    <br>
-
-                    <p><b>2 Hour Adult Open Gym</b></p>
-                    <ul>
-                        <li>$10 for Members</li>
-                        <li>$100 Member Punch Card (11 Visits)</li>
-                        <li>$15 for Non-Members</li>
-                        <li>$150 for Non-Member Punch Card (11 Visits)</li>
-                    </ul>
+                    <p>A final headcount must be given 1 week before scheduled field trip to be able to accommodate coaching staff.</p>
                     <br>
                 </div>
                 <div class="divider-vertical w-1 h-100% bg-neutral-600 ml-1 mr-12" />
-                <div class="w-11/12">
+                <div class="w-11/12 flex flex-wrap gap-4">
                     <!-- Right Side of Block -->
-                    <p class="underline">How to Register for a TRIAL class:</p>
-                    <ul>
-                        <li>Foam Pit</li>
-                        <li>Use of Most Gymnastics Equipment</li>
-                        <li>Ninja Warrior Obstacle Course</li>
-                        <li>Aerial Silks</li>
-                        <li>Dance Studio Space</li>
-                        <li>Baby Gym (Lil' Foot ONLY)</li>
-                        <li>Coach Supervision</li>
-                    </ul>
-                    <br />
-
-                    <p>*Coaches will NOT spot students during Open Gym. Open gym is designed for free play, not class time.</p>
-                    <br />
-                    <p>Children ages 14 years and younger should be accompanied by an adult in the building. Please do not drop off. Participation is not required.</p>
-                    <br />
-                    <p class="underline text-bold">All participants must create a parent portal before attending.</p>
+                    <div>
+                        <p><b>Field Trips Include:</b></p>
+                        <ul class="list-disc pl-6">
+                            <li><p>Open Gym Time</p></li>
+                            <li><p>Games & Activities</p></li>
+                            <li><p>Craft Activities</p></li>
+                            <li><p>Structured Gymnastics, Dance, Cheer or Ninja Instruction</p></li>
+                            <li><p>Obstacle Courses & Challenges</p></li>
+                            <li><p>Contests</p></li>
+                            <li><p>Trampoline</p></li>
+                            <li><p>Bounce House</p></li>
+                        </ul>
+                    </div>
+                    <!-- <img alt="Birthday Gifts" src="$lib/Regular/SpecialEvents/justparty.jpg"> -->
                 </div>
             </div>
         </div>
