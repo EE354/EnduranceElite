4d48b541cfc4aa16f10ba1caf3baaa08a8aab256
cmbackflip <colem1700@gmail.com>
2023-07-30 12:08:35 -0600

    change project name, minor updates

--- src/routes/(app)/calendar/+page.server.js
+++ src/routes/(app)/calendar/+page.server.js
@@ -1,20 +1,17 @@
 import {Event} from '$lib/server/models/Event';
 
 const formatEvent = (event) => {
-    /* TODO: implement a better color picking system*/
-    //Generate a number between 0 and 255
-    const genColor = () => Math.floor(Math.random() * 6);
     return {
         resourceId: event.id,
         title: event.name,
         start: new Date(event.timeStamp.start),
         end: new Date(event.timeStamp.end),
-        allDay: event.allDay,
+        allDay: false,
         extendedProps: {
             description: event.description,
             location: event.location,
         },
-        backgroundColor: `rgba(var(--color-secondary-${900}) / 1);`,
+        backgroundColor: `rgba(var(--color-primary-${900}) / 1);`,
     };
 };
 
@@ -23,7 +20,9 @@ export const load = async ({locals}) => {
     try {
         const {session, user} = locals.auth.validateUser();
 
-        const events = await Event.find();
+        const events = await Event.find({
+            group: "",
+        });
         let formattedEvents = [];
         for (let event of events) {
             formattedEvents.push(formatEvent(event));
