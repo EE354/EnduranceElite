fc47487399f628d089ac2d1506b52ec932683a52
PiSauce <pisauce@hotmail.com>
2023-08-01 08:41:09 -0600

    added validation, pasted in edit from schedule

--- src/routes/(admin)/admin/schedule/+page.server.js
+++ src/routes/(admin)/admin/schedule/+page.server.js
@@ -1,15 +1,23 @@
 import { nameSchema, stringSchema, eventDateSchema } from "$lib/server/zodSchemas.js";
 import {Schedule} from "$lib/server/models/Schedule";
+import {protectRoute} from "$lib/utils.js";
 
 
-export const load = async () => {
+export const load = async ({url, locals}) => {
+    const {session, user} = await locals.auth.validateUser();
+    protectRoute(url, user, session, 3)
 
+    return {
+
+    }
 }
 
 
 export const actions = {
     create: async ({locals, request}) => {
         const {session, user} = await locals.auth.validateUser();
+
+        protectRoute(url, user, session, 3);
 
         const form = await request.formData();
         const start = new Date(form.get("start"));
@@ -46,6 +54,8 @@ export const actions = {
     delete: async({locals, request}) => {
         const {session, user} = await locals.auth.validateUser();
 
+        protectRoute(url, user, session, 3);
+
         const form = await request.formData();
         const id = form.get("id");
 
@@ -64,10 +74,45 @@ export const actions = {
         }
     },
     edit: async({locals, request}) => {
-        //TODO: implement update
         const {session, user} = await locals.auth.validateUser();
 
-        const form = await request.formData();
+        protectRoute(url, user, session, 3)
 
+        const form = await request.formData();
+        const id = form.get("id");
+        const start = new Date(form.get("start"));
+        const end = new Date(form.get("end"));
+        const name = form.get("employee");
+        const description = form.get("description");
+
+        if (start > end) throw new Error("Start date must be before end date");
+
+        try {
+            eventDateSchema.parse(start);
+            eventDateSchema.parse(end);
+            nameSchema.parse(name);
+            stringSchema.parse(description);
+            stringSchema.parse(location);
+
+            const schedule = await Schedule.findById(id);
+
+            schedule.timeStamp.start = start;
+            schedule.timeStamp.end = end;
+            schedule.name = name;
+            schedule.description = description;
+            schedule.location = location;
+
+            await schedule.save();
+
+            return {
+                status: 200,
+                message: "Schedule updated successfully",
+            }
+        } catch (e) {
+            return {
+                status: 400,
+                message: e.message,
+            }
+        }
     }
 }
