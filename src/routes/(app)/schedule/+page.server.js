aa91fdadd3be751619c04c696523424aafa1023e
PiSauce <pisauce@hotmail.com>
2023-08-01 09:46:58 -0600

    removed unused values in formatSchedule

--- src/routes/(app)/schedule/+page.server.js
+++ src/routes/(app)/schedule/+page.server.js
@@ -2,21 +2,15 @@ import {Schedule} from '$lib/server/models/Schedule';
 import {protectRoute} from '$lib/utils';
 
 const formatSchedule = (schedule) => {
-    /* TODO: implement a better color picking system*/
-    //Generate a number between 0 and 255
-    const genColor = () => Math.floor(Math.random() * 256);
     return {
         resourceId: schedule.id,
-        title: schedule.name,
-        name: schedule.employee,
+        employee: schedule.employee,
         start: new Date(schedule.timeStamp.start),
         end: new Date(schedule.timeStamp.end),
-        allDay: schedule.allDay,
         extendedProps: {
             description: schedule.description,
-            location: schedule.location,
         },
-        backgroundColor: `rgb(${genColor()}, ${genColor()}, ${genColor()})`,
+        backgroundColor: `rgba(var(--color-primary-${900}) / 1);`,
     };
 };
 
